165ff71438ddc42dcbbf773cfdbc45eb0a343ec9
Jack Crawford <jack@Jacks-MacBook-Pro-2.local>
2019-11-02 11:11:39 -0400

    changes

--- tree.py
+++ tree.py
@@ -14,13 +14,50 @@ def getDescendents(subject):
     if(subject not in people):
         return subject
     descendents = people[subject]
-    print(descendents)
+    # print(descendents)
     for kid in descendents:
         if(kid in people):
             descendents.extend(getDescendents(kid))
     return descendents
-def getCousins(subject):
+def getAllCousins(subject):
     print("here we get", subject, "'s cousins")
+    ancs = getAncestors(subject)
+    decs = getDescendents(subject)
+    directLine = []
+    directLine.extend(ancs)
+    directLine.extend(decs)
+
+    potentialCousins = []
+    for cuz in directLine:
+        potentialCousins.extend(getDescendents(cuz))
+    cousins = []
+    for cuz in potentialCousins:
+        if cuz not in ancs and cuz not in decs and cuz not in cousins:
+            cousins.append(cuz)
+    return cousins
+def getCousinDegree(subject, potentialCousin):
+    subjectAncs = getAncestors(subject)
+    potentialAncs = getAncestors(potentialCousin)
+    subjectCt = potCt = 0
+    commonLink = False
+    subjectDegree = potentialDegree = 0
+    while not commonLink and subjectCt < len(subjectAncs):
+        while potCt < len(potentialAncs):
+            if(subjectAnc(subjectCt) is potentialAnc(potCt)):
+                # this is their closest common ancestor!
+                subjectDegree = subjectCt
+                potentialDegree = potCt
+                commonLink = True
+                break
+            potCt += 1
+        subjectCt += 1
+    cousinDegree = 0
+    if subjectCt == potCt:
+        cousinDegree = subjectCt/2
+    elif subjectCt > potCt:
+        cousinDegree = potCt/2
+    else:
+        cousinDegree = subjectCt/2
 for line in fileHandle:
     command = line[0]
     contents = line.split()
@@ -48,13 +85,16 @@ for line in fileHandle:
                     for potentialSibling in people[parent]:
                         if(potentialSibling != subject and potentialSibling not in siblings):
                             siblings.append(potentialSibling)
-            print("the siblings are", ", ".join(siblings))
+            print(subject, "-> siblings ->", ", ".join(siblings))
         if(key == "ancestor"):
             ancestors = getAncestors(subject)
-            print("the ancestors are", ", ".join(ancestors))
+            print(subject, "-> ancestors ->", ", ".join(ancestors))
         if(key == "descendent"):
             descendents = getDescendents(subject)
-            print("the descendents are", ", ".join(descendents))
+            print(subject, "-> descendents ->", ", ".join(descendents))
+        if(key == "cousin"):
+            cousins = getAllCousins(subject)
+            print(subject, "-> cousins -> ", ", ".join(cousins))
     if(command == "X"):
         subject = contents[3]
         personInQuestion = contents[1]
@@ -77,6 +117,11 @@ for line in fileHandle:
                 print(personInQuestion, "is a descendent of", subject)
             else:
                 print(personInQuestion, "is NOT a descendent of", subject)  
+        if(quality == "cousins"):
+            print("TRYIN COUSINS")
+            degree = contents[3]
+            degreeFound = getCousinDegree(subject, personInQuestion)
+            print(degreeFound)
         if(quality == "sibling"):
             siblings = []
             for parent, child in people.items():
